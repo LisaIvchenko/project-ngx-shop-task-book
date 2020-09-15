201e96734cb4927eec0e6da450545e2615043b91
Elizaveta <lisichenko@gmail.com>
2020-09-15 11:06:16 +0300

    finished 1-3

--- 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
+++ 1-components/3-category-product-card/src/app/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
 import {
   Component,
-  ChangeDetectionStrategy,
+  ChangeDetectionStrategy, Input, Output, EventEmitter,
 } from '@angular/core';
+import {IProduct} from '../../../../../shared/mocks/1-components/product';
 
 @Component({
   selector: 'ngx-shop-content-product',
@@ -9,6 +10,15 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoryProductComponent {
+  @Input() public product = {} as IProduct;
+  @Output() public addToCart: EventEmitter<any> = new EventEmitter<any>();
+  @Output() public goToProduct: EventEmitter<any> = new EventEmitter<any>();
 
+  public addToBasket(): void {
+    this.addToCart.emit();
+  }
 
+  public redirectTo(): void {
+    this.goToProduct.emit();
+  }
 }
